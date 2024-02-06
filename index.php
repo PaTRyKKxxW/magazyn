3db367ad1b86d72b5a9fd33cdb1330bac99bf36a
Patryk Wiktor <patryk141wiktor@interia.pl>
2024-02-06 13:47:57 +0100

    adding meta tags

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Strona magazynu do portfolio">
+    <meta name="keywords" content="magazyn, sklep, portfolio, Front-End, programowanie, warehause, shop">
+    <meta name="author" content="Patryk Wiktor">
     <title>Magazyn</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
 </body>
 </html>
