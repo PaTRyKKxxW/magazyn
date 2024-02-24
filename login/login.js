dc269249ec3ea618e5fb809d20b86958cdcf1ef0
Patryk Wiktor <patrykkxxw@Patryks-MBP.lan>
2024-02-24 15:24:00 +0100

    prettier

--- login/login.js
+++ login/login.js
@@ -1,10 +1,13 @@
-function result(){
-    let login = document.getElementById("login").value;
-    let pass = document.getElementById("pass").value;
-    if (login === "admin" && pass === "1324"){
-        alert("logged in as admin");
-        window.open = '../admin/indexadmin.html';
-    }else{
-        alert("try again");
-    }
-}
+let btn = document.querySelector(`#btn`);
+
+let result = () => {
+  let login = document.getElementById("login").value;
+  let pass = document.getElementById("pass").value;
+  if (login === "admin" && pass === "1324") {
+    window.location.href = "../admin/indexadmin.html";
+    alert("logged in as admin");
+  } else {
+    alert("try again");
+  }
+};
+btn.addEventListener(`click`, result);
